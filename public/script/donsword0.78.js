304156cbadf15bc44b9ca0c7504c494a55afa247
basedels13 <basedels13@gmail.com>
2026-07-27 16:57:24 +0900

    PWAテスト

--- public/script/donsword0.78.js
+++ public/script/donsword0.78.js
@@ -13723,3 +13723,9 @@ function alertButton(parent,text){
       }
     }   
   };
+  //PWA
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(() => console.log("Service Worker Registered"))
+        .catch(err => console.log(err));
+}
